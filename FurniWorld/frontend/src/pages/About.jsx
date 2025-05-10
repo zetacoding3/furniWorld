1ac9f2d6cf360eafe9136320b2c4c6830ea53837
zetacoding3 <zetacoding.academics@gmail.com>
2025-05-10 18:16:10 +0530

    initial commit

---
+++ FurniWorld/frontend/src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container-fluid g-0 px-3 py-5">
+                <img src="/img/about.webp" alt="" className="img-fluid col-md-4 float-start mx-4" />
+                <h1 className="text-center fw-semibold">Our Story</h1>
+                <p className='fs-5'>
+                    FurniWorld is your one-stop destination for stylish, durable, and affordable furniture that transforms houses into homes. Whether you're furnishing a cozy apartment or upgrading your dream home, we offer a wide range of contemporary and classic pieces that blend form, function, and flair.
+                </p>
+                <p className='fs-5'>
+                    From living rooms to bedrooms, dining spaces to workspaces — our curated collection includes sofas, beds, dining sets, office chairs, storage units, and more. Every piece is crafted with quality materials and thoughtful design to suit your lifestyle and taste.
+                </p>
+                <p className='fs-5'>
+                    At FurniWorld, we believe that great furniture isn’t just about looks — it’s about how it makes you feel. That’s why we combine comfort, craftsmanship, and customer care to deliver an exceptional experience, right from browsing to delivery.
+                </p>
+                <p className='fs-5'>
+                    FurniWorld – where comfort meets style, and homes come alive.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
